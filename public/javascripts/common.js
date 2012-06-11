adc7b84148c9292347f3efccf8fcc7018daad0f8
Oskar Krawczyk <oskar.krawczyk@gmail.com>
2012-06-11 16:02:31 +0200

    Tabs -> Spaces 2

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -2,28 +2,28 @@
 
  {description}
 
- @version	1.0
- @author	Oskar Krawczyk (thereis@nouincolor.com)
+ @version  1.0
+ @author Oskar Krawczyk (thereis@nouincolor.com)
 
 */
 
 (function(){
 
-	var Common = this.Common = new Class({
-		
-		Implements: [Options, Events],
-		
-		options: {},
-		
-		initialize: function(options){
-			this.setOptions(options);
-			this.setup();
-		},
-		
-		setup: function(){
-			console.log('Boing!');
-		}
-		
-	});
+ var Common = this.Common = new Class({
+   
+   Implements: [Options, Events],
+   
+   options: {},
+   
+   initialize: function(options){
+     this.setOptions(options);
+     this.setup();
+   },
+   
+   setup: function(){
+     console.log('Boing!');
+   }
+   
+ });
 
 })();
--- public/javascripts/extensions.js
+++ public/javascripts/extensions.js
@@ -2,56 +2,56 @@
 
  Common object extensions
 
- @version	1.0
- @author	Oskar Krawczyk (thereis@nouincolor.com)
+ @version  1.0
+ @author Oskar Krawczyk (thereis@nouincolor.com)
 
 */
 
 (function(){
 
-	Window.implement({
-		$log: function(){
-			if (typeof console !== 'undefined' && typeof console.log !== 'undefined'){
-				console.log(arguments.length <= 1 ? arguments[0] : arguments);
-			}
-		}
-	});
-	
-	Elements.implement({
-		
-		placeholder: function(fromAttr){
-			var events = {
-				focus: function(){
-					if (this.get('value') === this.get(fromAttr)){
-						this.set('value', '').removeClass('placeholder');
-					}
-				},
+ Window.implement({
+   $log: function(){
+     if (typeof console !== 'undefined' && typeof console.log !== 'undefined'){
+       console.log(arguments.length <= 1 ? arguments[0] : arguments);
+     }
+   }
+ });
+ 
+ Elements.implement({
+   
+   placeholder: function(fromAttr){
+     var events = {
+       focus: function(){
+         if (this.get('value') === this.get(fromAttr)){
+           this.set('value', '').removeClass('placeholder');
+         }
+       },
 
-				blur: function(){
-					if (this.get('value').trim() === ''){
-						this.set('value', this.get(fromAttr)).addClass('placeholder');
-					}
-				}
-			};
-			
-			var setup = function(input){
-				input.set('value', input.get(fromAttr)).addClass('placeholder');
-				input.addEvents(events);
-			};
-			this.each(setup);
-		}
-	});
-	
-	// Add a custom Validator to take in account the placeholder value
-	Form.Validator.add('withPlaceholder', {
-		errorMsg: false,
-		test: function(element, props){
-			if (element.get('data-placeholder') === element.get('value')){
-				return false;
-			} else {
-				return true;
-			}
-		}
-	});
+       blur: function(){
+         if (this.get('value').trim() === ''){
+           this.set('value', this.get(fromAttr)).addClass('placeholder');
+         }
+       }
+     };
+     
+     var setup = function(input){
+       input.set('value', input.get(fromAttr)).addClass('placeholder');
+       input.addEvents(events);
+     };
+     this.each(setup);
+   }
+ });
+ 
+ // Add a custom Validator to take in account the placeholder value
+ Form.Validator.add('withPlaceholder', {
+   errorMsg: false,
+   test: function(element, props){
+     if (element.get('data-placeholder') === element.get('value')){
+       return false;
+     } else {
+       return true;
+     }
+   }
+ });
 
 })();
